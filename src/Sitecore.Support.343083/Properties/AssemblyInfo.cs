303b7623e46ccf054b5eb0a32767b201d3a05956
ShcherbakAndrii <t22304@gmail.com>
2019-07-12 10:33:25 +0300

    Tested

---
+++ src/Sitecore.Support.343083/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.343083")]
+[assembly: AssemblyProduct("Sitecore.Support.343083")]
+[assembly: ComVisible(false)]
